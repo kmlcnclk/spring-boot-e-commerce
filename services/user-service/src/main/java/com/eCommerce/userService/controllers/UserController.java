83de5270d72e81c141fd22fbc3003477b5944702
Kamilcan Çelik <kamilcancelik5757@gmail.com>
2025-11-22 18:11:48 +0300

    add: rate limiter

--- services/user-service/src/main/java/com/eCommerce/userService/controllers/UserController.java
+++ services/user-service/src/main/java/com/eCommerce/userService/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.eCommerce.userService.controllers;
 import com.eCommerce.common.domain.User;
 import com.eCommerce.userService.dtos.CreateUserDto;
 import com.eCommerce.userService.services.UserService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +18,7 @@ public class UserController {
     private final UserService service;
 
     @PostMapping
+    @RateLimiter(name = "userRateLimiter")
     public User createUser(@RequestBody @Valid CreateUserDto user) {
         return service.saveUser(user);
     }
